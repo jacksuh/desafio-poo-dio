3077baa82963fa6471ef95e963c606e78dcd74a0
jacksuh <jacksonsdss@gmail.com>
2022-12-01 19:32:56 -0300

    Herança e polimorfismo

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,60 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+//Set - permitie adicionar apenas elementos unicos.
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudoInscritos = new HashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new HashSet<>();
+
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoInscritos() {
+        return conteudoInscritos;
+    }
+
+    public void setConteudoInscritos(Set<Conteudo> conteudoInscritos) {
+        this.conteudoInscritos = conteudoInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoInscritos, dev.conteudoInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudoInscritos, conteudosConcluidos);
+    }
+}
